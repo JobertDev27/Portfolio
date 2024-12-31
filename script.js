9abfe8b6fe9b93371a66ed5347f4443b9f02dee6
JobertDev27 <jobertdev27@gmail.com>
2024-12-31 19:35:52 +0800

    added projects

---
+++ script.js
@@ -0,0 +1,24 @@
+// const observer = new IntersectionObserver((entries) => {
+//     entries.forEach((entry) => {
+//         console.log(entry)
+//         if (entry.isIntersecting) {
+//             entry.target.classList.add("show")
+//         }
+//     });
+// });
+
+// const hiddenElements = document.querySelectorAll (".project-section");
+// hiddenElements.forEach((el) => observer.observe(el));
+
+document.addEventListener("DOMContentLoaded", () => {
+    const observer = new IntersectionObserver((entries) => {
+        entries.forEach((entry) => {
+            if (entry.isIntersecting) {
+                entry.target.classList.add("show");
+            }
+        });
+    });
+
+    const hiddenElements = document.querySelectorAll(".project-section");
+    hiddenElements.forEach((el) => observer.observe(el));
+});
